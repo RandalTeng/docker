fce93280b36698b0d4af06d8e3eeb1f994754a7b
randal <teng950916@gmail.com>
2018-05-07 12:08:12 +0800

    添加修改权限

--- php/docker-run.sh
+++ php/docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run -d \
 	-p 10559:9000 \
-	-v /code/docker/php/etc:/usr/local/php/etc \
-	-v /code/hanmaker_coe:/code/hanmaker_coe \
+	-v /code/docker/php/etc:/usr/local/php/etc:ro \
+	-v /code/hanmaker_coe:/code/hanmaker_coe:ro \
 	--name php559 \
 	t895469945/php:5.5.9-fpm
